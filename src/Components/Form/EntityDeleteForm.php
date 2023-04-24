de0841ffecb1138a36b02d5532d9d3914e7580ca
tarasmoroz <taras.moroz@ekreative.com>
2023-04-24 17:54:56 +0200

    Delete twitter

---
+++ src/Components/Form/EntityDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Components\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class EntityDeleteForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('delete', SubmitType::class);
+    }
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,8 +14,6 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'app_default')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->redirectToRoute('web_twitter_list');
     }
 }
