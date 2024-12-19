4fa18042487e4fba09469feca11c9c3d698decc5
tarasmoroz <taras.moroz@ekreative.com>
2024-12-19 12:35:06 +0100

    Added Friend Fixtures

---
+++ src/DataFixtures/FriendFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Entity\Friend;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Ramsey\Uuid\Nonstandard\Uuid;
+
+class FriendFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+
+        // Get references from users
+        /** @var User $admin */
+        $admin = $this->getReference(UserFixtures::USER_ADMIN);
+        /** @var User $user */
+        $user = $this->getReference(UserFixtures::USER);
+
+        $friend = (new Friend())
+            ->setUser($user)
+            ->setFriend($admin)
+            ->setStatus(0);
+
+        $manager->persist($friend);
+
+        $manager->flush();
+    }
+
+    // Depend on User
+    public function getDependencies(): array
+    {
+        return [
+            UserFixtures::class,
+        ];
+    }
+}
